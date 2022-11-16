624a383b0f98da73eb2bbd0e2cacb6080643e320
Susan <susandu924@gmail.com>
2022-11-16 16:18:25 -0600

    part 2 not doing what it should

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,6 +1,45 @@
 package org.launchcode.techjobs.persistent.models;
 
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
 public class Employer extends AbstractEntity {
 
+    @NotBlank(message = "Name is required")
+    @Size(min = 3, max = 50, message = "Name must be between 3 and 50 characters")
+    private String name;
 
+
+    @NotBlank(message = "Location is required")
+    @Size(min = 3, max = 100, message = "Location must be between 3 and 50 characters")
+    private String location;
+
+    public Employer(String name, String location) {
+        this.name = name;
+        this.location = location;
+    }
+
+    public Employer() {
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
